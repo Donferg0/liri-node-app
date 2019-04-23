c351eb78dc165c3417a9d88d04fb05e0fe9b6a22
BlazePhoenix <donnel.ferguson@hotmail.com>
2019-04-23 10:07:48 -0400

    If/Esle statement to determine which function  to run

--- liri.js
+++ liri.js
@@ -9,32 +9,37 @@ var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 
 
+if (process.argv[2] === "concert-this") {
+  var artist 
+  var queryURL = "https://rest.bandsintown.com/artists/DMX/events?app_id=codingbootcamp"
+  axios.get(queryURL).then(function(response) {
+    var events = response.data
+    console.log(events)
+  })
 
-var artist 
-var queryURL = "https://rest.bandsintown.com/artists/DMX/events?app_id=codingbootcamp"
-axios.get(queryURL).then(function(response) {
-  var events = response.data
-  // console.log(events)
-})
- 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
+  } else if (process.argv[2] === "spotify-this-song") {
+    spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+        if (err) {
+        return console.log('Error occurred: ' + err);
   }
  
-// console.log(data);
-});
+      console.log(data);
+      });
 
-var movie 
-var qURL = "http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy"
-axios.get(qURL).then(function(response) {
-  var movieInfo = response.data
-  // console.log("Title: " + movieInfo.Title);
-  // console.log("Year: " + movieInfo.Year);
-  // console.log("IMDB Rating: " + movieInfo.imdbRating);
-  // console.log("Rotten Tomatoes Rating: " + movieInfo.Ratings[1].Value);
-  // console.log("Country: " + movieInfo.Country);
-  // console.log("Language: " + movieInfo.Language);
-  // console.log("Plot: " + movieInfo.Plot);
-  // console.log("Actors: " + movieInfo.Actors);
-})
+  } else if (process.argv[2] === "movie-this") {
+    var movie 
+    var qURL = "http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy"
+    axios.get(qURL).then(function(response) {
+    var movieInfo = response.data
+    console.log("Title: " + movieInfo.Title);
+    console.log("Year: " + movieInfo.Year);
+    console.log("IMDB Rating: " + movieInfo.imdbRating);
+    console.log("Rotten Tomatoes Rating: " + movieInfo.Ratings[1].Value);
+    console.log("Country: " + movieInfo.Country);
+    console.log("Language: " + movieInfo.Language);
+    console.log("Plot: " + movieInfo.Plot);
+    console.log("Actors: " + movieInfo.Actors);
+  })
+} else {
+  console.log("Please input what you are searching for!")
+}
