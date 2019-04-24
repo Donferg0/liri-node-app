e4f248e5a4a396da083417c5f2851da6381b02e3
BlazePhoenix <donnel.ferguson@hotmail.com>
2019-04-24 13:43:49 -0400

    Set functions to take an argument. Completed Do-what-it-says function.

--- liri.js
+++ liri.js
@@ -12,8 +12,8 @@ var moment = require("moment");
 
 var fs = require("fs");
 
-function concert() {
-  var artist = process.argv.slice(3).join(" ");
+function concert(query) {
+  var artist = query || process.argv.slice(3).join(" ");
   if (artist === "") {
       artist = "DMX"
   }
@@ -31,8 +31,8 @@ function concert() {
     }
 })
 }
-function spotifyhere() {
-  var track = process.argv.slice(3).join(" ");
+function spotifyhere(query) {
+  var track = query || process.argv.slice(3).join(" ");
   if (track === "") {
       track =  "Ignition"
   }
@@ -43,7 +43,10 @@ function spotifyhere() {
       }
       var song = data.tracks.items
 
+
+
       for (var i = 0; i < song.length; i++) {
+      for ( var j = 0; j < song[i].artists.length; j++)
       console.log("Artists: " + song[i].artists[0].name)
       console.log("Song Name: " + song[i].name)
       console.log("Preview: " + song[i].preview_url)
@@ -53,8 +56,8 @@ function spotifyhere() {
     });
 
 }
-function moviethis() {
-  var movie = process.argv.slice(3).join(" ");
+function moviethis(query) {
+  var movie = query || process.argv.slice(3).join(" ");
   if (movie === "") {
       movie = "Underworld"
   }
@@ -82,60 +85,11 @@ function dowhatitsays() {
 
     var dataArr = data.split(",");
     if (dataArr[0] === "concert-this") {
-                var artist = dataArr[1];
-                if (artist === "") {
-                    artist = "DMX"
-                }
-                var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-                axios.get(queryURL).then(function(response) {
-                  var events = response.data
-                  if (!events.length) {
-                          console.log("No events found for: "  + artist)
-                  }
-                  for (var i = 0; i < events.length; i++) {
-                          console.log("Name of Venue: " + events[i].venue.name)
-                          console.log("Venue Location: " + events[i].venue.city + ", " + events[i].venue.region + ", " + events[i].venue.country)
-                          console.log("Date of the Event: " + moment(events[i].datetime).format("MM/DD/YYYY"))
-                          console.log("")
-                  }
-              })
+        concert(dataArr[1])
     } else if (dataArr[0] === "spotify-this-song") {
-          var track = dataArr[1];
-          if (track === "") {
-              track =  "Ignition"
-          }
-        
-          spotify.search({ type: 'track' , query: track , limit: 5 }, function(err, data) {
-              if (err) {
-              return console.log('Error occurred: ' + err);
-              }
-              var song = data.tracks.items
-        
-              for (var i = 0; i < song.length; i++) {
-              console.log("Artists: " + song[i].artists[0].name)
-              console.log("Song Name: " + song[i].name)
-              console.log("Preview: " + song[i].preview_url)
-              console.log("Album: " + song[i].album.name)
-              console.log("")
-              }
-            });
+          spotifyhere(dataArr[1])
     } else if (dataArr[0] === "movie-this") {
-        var movie = dataArr[1];
-        if (movie === "") {
-            movie = "Underworld"
-        }
-        var qURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy"
-        axios.get(qURL).then(function(response) {
-          var movieInfo = response.data
-          console.log("Title: " + movieInfo.Title);
-          console.log("Year: " + movieInfo.Year);
-          console.log("IMDB Rating: " + movieInfo.imdbRating);
-          console.log("Rotten Tomatoes Rating: " + movieInfo.Ratings[1].Value);
-          console.log("Country: " + movieInfo.Country);
-          console.log("Language: " + movieInfo.Language);
-          console.log("Plot: " + movieInfo.Plot);
-          console.log("Actors: " + movieInfo.Actors);
-        })
+          moviethis(dataArr[1])
       }
     });
 }
